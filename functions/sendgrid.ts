0b58dd7ae8f36a98d4385aee05bbe6e5d59f1897
Lyndon Maydwell <lyndon@sordina.net>
2023-09-16 15:56:27 +1000

    function docs

--- functions/sendgrid.ts
+++ functions/sendgrid.ts
@@ -1,5 +1,15 @@
 import { sendSimpleMail, IResult } from "https://deno.land/x/sendgrid/mod.ts";
 
+/**
+ * Uses the SendGrid API to send an email.
+ * 
+ * @param subject The email subject
+ * @param to The address to send the email to
+ * @param from Who to list as the sender of the email
+ * @param plain The plaintext content of the email
+ * @param html The HTML content of the email
+ * @returns Success or errors
+ */
 export async function send(subject: string, to: string, from: string, plain: string, html: string ): Promise<IResult> {
 	const API_KEY = Deno.env.get("SENDGRID_API_KEY");
 
